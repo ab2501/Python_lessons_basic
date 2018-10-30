38eaa96f3109ad654810e12dd5fd50e199efccb8
ab2501 <rorsha@gmail.com>
2018-10-30 21:18:38 +0300

    DZ lesson 4 a.butarev
    Hard  - 1 и 2 задания сделаны.

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -3,6 +3,9 @@
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+##
+
+import re
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -21,13 +24,37 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
 
+
+# 1. Решение с помощью re
+line_str = re.findall(r'[a-z]+', line)
+print("Итого: \n",line_str)
+
+
+# 2. Решение без re (с помощью знакомых)
+symbol = list(map(lambda x: chr(x), list(range(65,91)))) # Преобразуем список из кодов ANSI в список букв A-Z
+line_new = list(line)
+
+for i, element in enumerate(line_new[:]):
+    for element_2 in symbol:
+        if element == element_2:
+            line_new[i] = ' '
+
+
+stroka=''.join(line_new).split(' ')
+
+line_str_2 = [i for i in stroka if i != '']
+print('Итого: \n',line_str_2)
+
+
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки 
+# Т.е. из строки
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+
+import re
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +72,69 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+# 1. Решение с помощью re
+
+line_2_str = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print('Итого: \n',line_2_str)
+
+# 2. Решение без re (так же с помощью знакомых)
+symbol1 = list(map(lambda x: chr(x), list(range(65, 91)))) # Преобразуем список из кодов ANSI в список букв A-Z
+symbol2 = list(map(lambda x: chr(x), list(range(97, 123)))) # Преобразуем список из кодов ANSI в список букв a-z
+line_new = list(line_2)
+
+lst = []
+i = len(line_new) - 1
+
+while i >= 0:
+    if line_new[i] in symbol2:
+         lst.append(line_new[i])
+    elif line_new[i] in symbol1 and i <= len(line_new) - 3 and line_new[i+1] in symbol1 and line_new[i+2] in symbol1:
+        lst.append(line_new[i])
+    else:
+        lst.append(' ')
+    i -= 1
+lst.reverse()
+
+i = 0
+lst2 = []
+registr = True
+
+while i <= len(lst)-1:
+    if lst[i] in symbol2:
+        registr = True
+    if lst[i] in symbol1 and lst[i-1] in symbol2 and lst[i-2] in symbol2:
+        lst2.append(lst[i])
+        registr = False
+    elif lst[i] in symbol1 and registr == False:
+        lst2.append(lst[i])
+    else:
+        lst2.append(' ')
+    i += 1
+stroka=''.join(lst2).split(' ')
+
+line_str_3 = [i for i in stroka if i != '']
+print('Итого: \n',line_str_3)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import re
+import random
+
+with open('Kirkudu.txt', 'w', encoding='UTF-8') as file:
+    n = [str(random.randint(0, 9)) for _ in range(2499)]
+    n = str(random.randint(1, 9)) + ''.join(n)
+    file.write(n)
+
+with open('Kirkudu.txt', 'r', encoding='UTF-8') as file:
+    result = ['0']
+    longnum = file.readline()
+    pattern = '([0]{2,}|[1]{2,}|[2]{2,}|[3]{2,}|[4]{2,}|' \
+              '[5]{2,}|[6]{2,}|[7]{2,}|[8]{2,}|[9]{2,})'
+    found = re.findall(pattern, longnum)
+    [result.insert(0, x) for x in found if len(x) > len(result[0])]
+    print(result.pop(0))
