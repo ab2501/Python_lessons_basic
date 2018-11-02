0cb5e06de2826799eb06c5c7b1a3af3cfd206476
ab2501 <rorsha@gmail.com>
2018-11-02 20:41:09 +0300

    DZ lesson 5 a.butarev

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,27 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import dir_com_hw05_ez as dc
+
+dirs = ['dir_' + str(n) for n in range(1, 10)]
+
+for cur_dir in dirs:
+    dc.make_dir(cur_dir)
+
+for cur_dir in dirs:
+    dc.del_dir(cur_dir)
+
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+import dir_com_hw05_ez as dc
+
+print(dc.dir_list())
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+import dir_com_hw05_ez as dc
+
+dc.copy_file()
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,48 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import dir_com_hw05_norm as dc
+
+
+def menu():
+    print('Выберете действие (1-4):')
+    print('1. Перейти в папку')
+    print('2. Просмотреть содержимое текущей папки')
+    print('3. Удалить папку')
+    print('4. Создать папку')
+    print('Для выхода введите "q"')
+    action = input()
+    return action
+
+
+def handler(action):
+    if action == 'q':
+        print('Выход из программы...')
+        print('Успешно')
+        return
+    action_item = {
+        '1': dc.change_dir,
+        '2': dc.dir_list,
+        '3': dc.del_dir,
+        '4': dc.make_dir
+        }
+    item = None
+    try:
+        item = int(action)
+        if item in range(1, 5):
+            for item, key in action_item.items():
+                if item == action:
+                    key()
+        else:
+            print('"{}" - некорректная команда!'.format(action))
+
+    except ValueError:
+        print('"{}" - некорректная команда!'.format(action))
+
+    answer = input('Продолжить работу с программой?\n"y" - Да: ')
+
+    handler(menu()) if answer == 'y' else handler('q')
+
+
+handler(menu())
