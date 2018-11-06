748c808d65d35b8da27df11c19fc5e5d679ae6aa
ab2501 <rorsha@gmail.com>
2018-11-06 21:43:54 +0300

    DZ lesson 6 a.butarev

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,88 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
+from math import sqrt
+
+
+class Side:
+    @staticmethod
+    def width(A, B):
+        return round(sqrt(sum(tuple(map(lambda a, b: (b - a)**2, A, B)))), 2)
+
+
+class Triangle(Side):
+    def __init__(self, A, B, C):
+        self._A = A
+        self._B = B
+        self._C = C
+
+    def sides(self):
+        return {'AB': self.width(self._A, self._B),
+                'BC': self.width(self._B, self._C),
+                'CA': self.width(self._C, self._A)
+                }
+
+    def perimeter(self):
+        return round(self.sides()['AB'] +
+                     self.sides()['BC'] +
+                     self.sides()['CA'], 2)
+
+    def area(self):
+        return round((lambda p, a, b, c:
+                      sqrt(p*(p - a)*(p - b)*(p - c)))
+                      (self.perimeter() / 2,
+                       self.sides()['AB'],
+                       self.sides()['BC'],
+                       self.sides()['CA']), 2)
+
+A1, A2, A3 = (7, -13), (-9, 9), (12, -11)
+
+ABC = Triangle(A1, A2, A3)
+
+print('В треугольнике со сторонами:')
+print(' см\n'.join([str(side) for side in ABC.sides().values()]) + ' cм')
+print('Площадь: {} кв.см'.format(ABC.area()))
+print('Периметр: {} см'.format(ABC.perimeter()))
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+import math
+
+
+class Trapeze:
+    def __init__(self, a, b, c, d):
+
+        try:
+            self.AB = round(math.sqrt((b[0]-a[0])**2 + (b[1]-a[1])**2), 2)
+            self.BC = round(math.sqrt((c[0]-b[0])**2 + (c[1]-b[1])**2), 2)
+            self.CD = round(math.sqrt((d[0]-c[0])**2 + (d[1]-c[1])**2), 2)
+            self.DA = round(math.sqrt((a[0]-d[0])**2 + (a[1]-d[1])**2), 2)
+        except TypeError:
+            print('Параметры заданы неправильно')
+
+    def isosceles(self):
+        return self.AB == self.CD
+
+    def perimeter(self):
+        return round(self.AB + self.BC + self.CD + self.DA, 2)
+
+    def area(self):
+        if self.isosceles():
+            h = math.sqrt(self.AB**2 - ((self.DA - self.BC)**2) / 4)
+            return round(1/2 * (self.BC + self.DA) * h, 2)
+        else:
+            p = self.perimeter() / 2
+            return round((self.BC + self.DA)/abs(self.DA - self.BC) *
+                         math.sqrt((p-self.DA)*(p-self.BC) *
+                                   (p-self.DA-self.AB) *
+                                   (p-self.DA-self.CD)), 2)
+
+
+trap = Trapeze((-7, 4), (-5, 6), (6, 6), (8, 5))
+
+print('Трапеция с основаниями DA и BC\nявляется равнобокой: {0}\n'
+      'с Площадью {1} кв.см и Периметром {2} '
+      'см'.format(trap.isosceles(), trap.area(), trap.perimeter()))
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -2,8 +2,8 @@
 # Реализуйте описаную ниже задачу, используя парадигмы ООП:
 # В школе есть Классы(5А, 7Б и т.д.), в которых учатся Ученики.
 # У каждого ученика есть два Родителя(мама и папа).
-# Также в школе преподают Учителя. Один учитель может преподавать 
-# в неограниченном кол-ве классов свой определенный предмет. 
+# Также в школе преподают Учителя. Один учитель может преподавать
+# в неограниченном кол-ве классов свой определенный предмет.
 # Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
 # но больше математику не может преподавать никто другой.
 
@@ -11,7 +11,192 @@
 # 1. Получить полный список всех классов школы
 # 2. Получить список всех учеников в указанном классе
 #  (каждый ученик отображается в формате "Фамилия И.О.")
-# 3. Получить список всех предметов указанного ученика 
+# 3. Получить список всех предметов указанного ученика
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+
+class School:
+    def __init__(self, school_name, school_adress, teachers, students):
+        self._school_name = school_name
+        self._school_adress = school_adress
+        self._teachers = teachers
+        self._students = students
+
+    def get_all_classes(self):
+        classes = set([student.get_class_room for student in self._students])
+        return list(sorted(classes, key=lambda x: int(x[:-1])))
+
+    def get_students(self, class_room):
+        return [student.get_short_name for student in self._students if
+                class_room == student.get_class_room]
+
+    def get_teachers(self, class_room):
+        return [teacher.get_short_name for teacher in self._teachers if
+                class_room in teacher.get_classes]
+
+    def find_student(self, student_full_name):
+        for person in self._students:
+            if student_full_name == person.get_full_name:
+                teachers = [teachers.get_short_name for teachers in
+                            self._teachers if person.get_class_room in
+                            teachers.get_classes]
+                lessons = [teachers.get_courses for teachers in
+                           self._teachers if person.get_class_room in
+                           teachers.get_classes]
+                parents = person.get_parents
+
+                return {
+                    'full_name': student_full_name,
+                    'class_room': person.get_class_room,
+                    'teachers': teachers,
+                    'lessons': lessons,
+                    'parents': parents
+                    }
+
+    @property
+    def name(self):
+        return ' ' \
+               '"{}"'.format(self._school_name)
+
+    @property
+    def adress(self):
+        return '{}'.format(self._school_adress)
+
+
+class People:
+    def __init__(self, last_name, first_name, middle_name):
+        self._last_name = last_name
+        self._first_name = first_name
+        self._middle_name = middle_name
+
+    @property
+    def get_full_name(self):
+        return '{0} {1} {2}'.format(self._last_name,
+                                    self._first_name,
+                                    self._middle_name)
+
+    @property
+    def get_short_name(self):
+        return '{0} {1}.{2}.'.format(self._last_name,
+                                     self._first_name[:1],
+                                     self._middle_name[:1])
+
+
+class Student(People):
+    def __init__(self, last_name, first_name, middle_name,
+                 class_room, mather, father):
+        People.__init__(self, last_name, first_name, middle_name)
+        self._class_room = class_room
+        self._parents = {
+            'mather': mather,
+            'father': father
+            }
+
+    @property
+    def get_class_room(self):
+        return self._class_room
+
+    @property
+    def get_parents(self):
+        return self._parents
+
+
+class Teacher(People):
+    def __init__(self, last_name, first_name, middle_name,
+                 courses, classes):
+        People.__init__(self, last_name, first_name, middle_name)
+        self._courses = courses
+        self._classes = classes
+
+    @property
+    def get_courses(self):
+        return self._courses
+
+    @property
+    def get_classes(self):
+        return self._classes
+
+
+teachers = [
+    Teacher('Жукова', 'Людмила', 'Александровна', 'Математика',
+            ['7А', '7Б', '8А', '8Б', '9А', '9Б', '10А', '10Б', '11А', '11Б']),
+    Teacher('Белик', 'Вячеслав', 'Валерьевич', 'Информатика',
+            ['10А', '10Б', '11А', '11Б']),
+    Teacher('Лакина', 'Надежда', 'Фёдоровна', 'История',
+            ['7А', '7Б', '8А', '8Б', '9А', '9Б', '10А', '10Б', '11А', '11Б']),
+    Teacher('Мордвинова', 'Татьяна', 'Владимировна', 'Литература',
+            ['7А', '7Б', '8А', '8Б', '9А', '9Б', '10А', '10Б', '11А', '11Б']),
+    Teacher('Лагута', 'Татьяна', 'Ивановна', 'География',
+            ['7А', '7Б', '8А', '8Б', '9А', '9Б'])
+    ]
+
+students = [
+    Student('Ковалев', 'Александр', 'Александрович', '10Б',
+            'Ковалева О. А.', 'Ковалев А. В.'),
+    Student('Юдин', 'Евгений', 'Алексеевич', '11А',
+            'Юдина Т.В.', 'Юдин А.В.'),
+    Student('Бахрушин', 'Константин', 'Сергеевич', '11Б',
+            'Бахрушина А.Д.', 'Бахрушин С.А.'),
+    Student('Звонарев', 'Виталий', 'Николаевич', '10А',
+            'Звонарева А.К.', 'Звонарев Н.В.'),
+    Student('Ю Тань', 'Артём', 'Александрович', '9Б',
+            'Ю Таньа В.А.', 'Ю Тань А.Т'),
+    Student('Котиков', 'Никита', 'Никитович', '9А',
+            'Котикова Н.А.', 'Котиков Н.С.'),
+    Student('Савельева', 'Дарья', 'Александровна', '8Б',
+            'Савельева А.В.', 'Савельев А.С.'),
+    Student('Штемц', 'Андрей', 'Васильевич', '8А',
+            'Штемц Е.В.', 'Штемц В.С.'),
+    Student('Андреева', 'Анна', 'Владимировна', '7Б',
+            'Андреева А.Д.', 'Андреев В.А.'),
+    Student('Зайцев', 'Михаил', 'Аркадьевич', '7А',
+            'Зайцева А.Г.', 'Зайцев А.С.'),
+    Student('Кулебякин', 'Александр', 'Сергеевич', '10Б',
+            'Кулебякина О. А.', 'Кулебякин С. В.'),
+    Student('Кулебякин', 'Алексей', 'Сергеевич', '11А',
+            'Кулебякина О. А.', 'Кулебякин С. В..'),
+    Student('Макарова', 'Ирина', 'Александровна', '11Б',
+            'Макарова А.Д.', 'Макаров А.А.'),
+    Student('Кравченко', 'Александр', 'Владимирович', '10А',
+            'Кравченко Е.К.', 'Кравченко В.В.'),
+    Student('Беляева', 'Мария', 'Вячеславовна', '9Б',
+            'Беляева В.А.', 'Беляев В.Т'),
+    Student('Рыбакова', 'Лидия', 'Константиновна', '9А',
+            'Рыбакова Н.А.', 'Рыбаков К.С.'),
+    Student('Абрамкин', 'Ярослав', 'Александрович', '8Б',
+            'Абрамкина А.В.', 'Абрамкин А.С.'),
+    Student('Кудякова', 'Анжелла', 'Васильевна', '8А',
+            'Кудякова Е.В.', 'Кудяков В.С.'),
+    Student('Дмитриева', 'Лилия', 'Владимировна', '7Б',
+            'Дмитриева А.Д.', 'Дмитриев В.А.'),
+    Student('Андросова', 'Анна', 'Сергеевна', '7А',
+            'Андросова А.Г.', 'Андросов С.С.'),
+    ]
+
+
+school = School('Школа Питона №1,', 'Луна, '
+                'Море Спокойствия', teachers, students)
+
+print(school.name)
+print(school.adress)
+
+print('\nСписок классов школы:')
+print(', '.join(school.get_all_classes()))
+
+print('\nСписок "10Б" класса:')
+print('\n'.join(school.get_students('10Б')))
+
+student = school.find_student('Ковалев Александр Александрович')
+print('\nУченик: {0}\nУчебный класс: "{1}"\n'
+      'Учителя: {2}\nПредметы: {3}'.format(student['full_name'],
+                                           student['class_room'],
+                                           ', '.join(student['teachers']),
+                                           ', '.join(student['lessons'])))
+
+print('Родители: {0}, {1}'.format(student['parents']['mather'],
+                                   student['parents']['father']))
+
+print('\nКласс: "8А"\nПреподаватели: '
+      '{0}'.format(', '.join(school.get_teachers('8А'))))
